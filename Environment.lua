262aa6eedcf958c12a058b892349e0e9e6a4df0a
ScorpionDevelopment <apzraz452@gmail.com>
2024-12-02 19:18:05 +0200

    Update Environment.lua

--- Environment.lua
+++ Environment.lua
@@ -605,27 +605,6 @@ function saveinstance()
 	synsaveinstance(SaveOptions)
 end
 
-local decompsrc = game:HttpGet("https://raw.githubusercontent.com/RazAPI/Slaze/main/Debug/x64/Init/InitScript.lua", true) 
-local function loaddecomp(decomptimeout)
-    local CONSTANTS = [[
-        local ENABLED_REMARKS = {
-            NATIVE_REMARK = true,
-            INLINE_REMARK = true
-        }
-        local DECOMPILER_TIMEOUT = ]] .. decomptimeout .. [[
-            
-        local READER_FLOAT_PRECISION = 99 -- up to 99
-        local SHOW_INSTRUCTION_LINES = false
-        local SHOW_REFERENCES = true
-        local SHOW_OPERATION_NAMES = false
-        local SHOW_MISC_OPERATIONS = true
-        local LIST_USED_GLOBALS = true
-        local RETURN_ELAPSED_TIME = false
-    ]]
-    loadstring(string.gsub(decompsrc, "-- CONSTANTS HERE --", CONSTANTS), "Advanced-Decompiler-V3")()
-end
-loaddecomp(10)
-
 function check(funcName: string, func, testfunc)
     local success, err = pcall(function()
         getgenv()[funcName] = func
